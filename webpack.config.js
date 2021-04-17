15350ebfe59c315c9fe816874e889aa2289fba0d
X00011999 <thomas.mayer@smail.auswaertiges-amt.de>
2021-04-17 14:21:19 +0200

    latest changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,17 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
+    entry: './src/client/index.js',
+    output: { 
+
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 }
