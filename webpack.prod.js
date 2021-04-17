2f23dedd580dd9c81e5c14401b51848307999624
X00011999 <thomas.mayer@smail.auswaertiges-amt.de>
2021-04-17 16:23:28 +0200

    first adaption towards including sass

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
+    devtool: 'source-map',
     entry: './src/client/app.js',
     output: { 
 
@@ -21,6 +23,15 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
 }
